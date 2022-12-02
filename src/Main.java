aedb488f8a02ed0530694ffa5ffd4cc0f72a4461
ElizavetaNoskova <wliza68@gmail.com>
2022-12-02 20:23:39 +0300

    1

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args) {
+        task1();
+        task2();
+    }
+
+    public static void task1 () {
+        System.out.println("Задача 1");
+        byte a = 3;
+        System.out.println("Значение переменной a с типом byte равно "+ a);
+        short b = 4;
+        System.out.println("Значение переменной b с типом short равно "+ b);
+        int c = 5;
+        System.out.println("Значение переменной c с типом int равно "+ c);
+        long d = 1000000;
+        System.out.println("Значение переменной d с типом long равно "+ d);
+    }
+
+    public static void task2 () {
+        System.out.println("Задача 2");
+
+    }
+}
